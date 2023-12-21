9d811880877e3c80353a8301e3cdc911fe84cb6d
unknown <morzmamun@gmail.com>
2023-12-22 01:44:56 +0600

    phone responsive done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -6,8 +6,10 @@ const Banner = () => {
       <div className="flex flex-col-reverse md:flex-row-reverse md:pt-10  max-w-screen-xl justify-center items-center mx-auto md:h-[600px]">
         <div className="md:w-1/2">
           <img src={banner} className="" />
+          <div className="flex items-center justify-center mb-5 md:hidden">
+            <button className="btn btn-outline text-warning hover:bg-white hover:text-black">LET'S EXPLORE <FaArrowRightFromBracket className="text-xl"></FaArrowRightFromBracket></button>
+          </div>
         </div>
-
         <div className="md:w-1/2 p-5">
           <h1 className="text-2xl md:text-5xl font-bold">
             Best Way to Manage <br /> Your Daily Tasks
@@ -17,7 +19,7 @@ const Banner = () => {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <div className="flex items-center justify-center mt-10">
+          <div className="mt-10 hidden md:block">
             <button className="btn btn-outline text-warning hover:bg-white hover:text-black">LET'S EXPLORE <FaArrowRightFromBracket className="text-xl"></FaArrowRightFromBracket></button>
           </div>
         </div>
