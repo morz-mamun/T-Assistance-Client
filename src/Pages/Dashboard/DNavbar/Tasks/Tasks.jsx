b81beece329d6841e3dd085c537772b43c0a69a1
unknown <morzmamun@gmail.com>
2023-12-24 00:55:49 +0600

    use react hook form to add task

--- src/Pages/Dashboard/DNavbar/Tasks/Tasks.jsx
+++ src/Pages/Dashboard/DNavbar/Tasks/Tasks.jsx
@@ -4,51 +4,54 @@ import { FcProcess } from "react-icons/fc";
 import { MdFileDownloadDone } from "react-icons/md";
 import { IoMdAdd } from "react-icons/io";
 import useAxios from "../../../../Hooks/useAxios";
+import Swal from "sweetalert2";
+import { useForm } from "react-hook-form";
+const Tasks = () => {
+  const axiosPublic = useAxios();
+  const { register, handleSubmit, reset } = useForm();
 
-const Tasks = () => {   
-    const axiosPublic = useAxios()
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top",
+    iconColor: "blue",
+    customClass: {
+      popup: "colored-toast",
+    },
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+  });
 
-    const Toast = Swal.mixin({
-        toast: true,
-        position: "top",
-        iconColor: "blue",
-        customClass: {
-          popup: "colored-toast",
-        },
-        showConfirmButton: false,
-        timer: 1500,
-        timerProgressBar: true,
-      });
-    
-  const handleAddTask = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const email = form.email.value;
-    const title = form.title.value;
-    const descriptions = form.descriptions.value;
-    const date_from = form.date_from.value;
-    const date_too = form.date_too.value;
-    const priority = form.priority.value;
+  const onSubmit = async (data) => {
+    console.log(data);
+    const taskInfo = {
+      email: data.email,
+      title: data.title,
+      descriptions: data.descriptions,
+      date_from: data.date_from,
+      date_too: data.date_too,
+      priority: data.priority,
+    };
 
-    const taskInfo = {email, title, descriptions, date_from, date_too, priority}
-
-    axiosPublic.post('/allTask', taskInfo)
-    .then((res) => {
-        if(res.data.insertedId){
-            Toast.fire({
-                icon: "success",
-                title: "User Registration Successfully.",
-              });
+    axiosPublic
+      .post("/allTask", taskInfo)
+      .then((res) => {
+        if (res.data.insertedId) {
+          reset();
+          Toast.fire({
+            icon: "success",
+            title: "User Registration Successfully.",
+          });
         }
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         Toast.fire({
-            icon: "error",
-            title: err.message,
-          });  
-    } )
-    
+          icon: "error",
+          title: err.message,
+        });
+      });
   };
+
   return (
     <div className="px-2 md:px-10 py-5 text-black">
       <div className="flex justify-between items-center">
@@ -73,83 +76,89 @@ const Tasks = () => {
                 ✕
               </button>
             </form>
-            <form  onSubmit={handleAddTask}  className="space-y-4 pt-5">
-                  <div className="space-y-2">
-                    <label className="text-base">
-                      <span className="">Requester Email</span>
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Type Here"
-                      className="border-2 p-2 input-ghost w-full"
-                    />
-                  </div>
 
-                  <div className="space-y-2">
-                    <label className="text-base">
-                      <span className="">Task Title</span>
-                    </label>
-                    <input
-                      type="text"
-                      name="title"
-                      placeholder="Type Here"
-                      className="border-2 p-2 input-ghost w-full"
-                    />
-                  </div>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 pt-5">
+              <div className="space-y-2">
+                <label className="text-base">
+                  <span className="">Requester Email*</span>
+                </label>
+                <input
+                  type="email"
+                  {...register("email", { required: true })}
+                  placeholder="Type Here"
+                  className="border-2 p-2 input-ghost w-full"
+                />
+              </div>
 
-                  <div className="space-y-2">
-                    <label className="text-base">
-                      <span className="">Descriptions</span>
-                    </label>
-                    <input
-                      type="text"
-                      name="descriptions"
-                      placeholder="Type Here"
-                      className="border-2 p-2 input-ghost w-full"
-                    />
-                  </div>
+              <div className="space-y-2">
+                <label className="text-base">
+                  <span className="">Task Title*</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("title", { required: true })}
+                  placeholder="Type Here"
+                  className="border-2 p-2 input-ghost w-full"
+                />
+              </div>
 
-                  <div className="space-y-2">
-                    <label className="text-base">
-                      <span className="">Deadline</span>
-                    </label>
-                    <div className="flex flex-col md:flex-row gap-1">
-                      <input
-                        type="date"
-                        name="date_from"
-                        placeholder=""
-                        className="border-2 p-2 input-ghost w-full"
-                      />
-                      <p className="flex items-center">Too</p>
-                      <input
-                        type="date"
-                        name="date_too"
-                        placeholder=""
-                        className="border-2 p-2 input-ghost w-full"
-                      />
-                    </div>
-                  </div>
+              <div className="space-y-2">
+                <label className="text-base">
+                  <span className="">Descriptions*</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("descriptions", { required: true })}
+                  placeholder="Type Here"
+                  className="border-2 p-2 input-ghost w-full"
+                />
+              </div>
 
-                  <div className="space-y-2">
-                    <label className="text-base">
-                      <span className="">Priority</span>
-                    </label>
-                    <select className="border-2 p-2 w-full" name="priority">
-                      <option>High</option>
-                      <option>Medium</option>
-                      <option>Low</option>
-                    </select>
-                  </div>
+              <div className="space-y-2">
+                <label className="text-base">
+                  <span className="">Deadline*</span>
+                </label>
+                <div className="flex flex-col md:flex-row gap-1">
+                  <input
+                    type="date"
+                    {...register("date_from", { required: true })}
+                    placeholder=""
+                    className="border-2 p-2 input-ghost w-full"
+                  />
+                  <p className="flex items-center">Too</p>
+                  <input
+                    type="date"
+                    {...register("date_too", { required: true })}
+                    placeholder=""
+                    className="border-2 p-2 input-ghost w-full"
+                  />
+                </div>
+              </div>
 
-                  <div className="flex items-center justify-center">
-                    <button
-                      className="btn btn-outline btn-sm bg-warning hover:bg-white hover:text-black"
-                    >
-                      Add Task
-                    </button>
-                  </div>
-                </form>
+              <div className="space-y-2">
+                <label className="text-base">
+                  <span className="">Priority*</span>
+                </label>
+                <select
+                  className="border-2 p-2 w-full"
+                  defaultValue="default"
+                  {...register("priority", { required: true })}
+                >
+                  <option disabled value="default">
+                    Select Priority
+                  </option>
+                  <option>High</option>
+                  <option>Medium</option>
+                  <option>Low</option>
+                </select>
+              </div>
+
+              <div className="flex items-center justify-center">
+                <button className="btn btn-outline btn-sm bg-warning hover:bg-white hover:text-black">
+                  Add Task
+                </button>
+              </div>
+            </form>
           </div>
         </dialog>
       </div>
