1655b25f24d8f7626fb1318a496ab71e68b4ee2e
morz <morzmamun@gmail.com>
2024-10-11 23:32:28 +0600

    hook added

---
+++ src/Hooks/useAxios.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const axiosPublic = axios.create({
+    // baseURL: 'https://task-management-platform-server-nine.vercel.app'
+    baseURL: 'http://localhost:5000'
+})
+
+const useAxios = () => {
+    return axiosPublic
+};
+
+export default useAxios;
