053b758f6c826a428f86fa7239e899416764e783
Md Morshed Alam <morzmamun@gmail.com>
2025-06-28 21:26:00 +0600

    Update useAxios.jsx

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    // baseURL: 'https://task-management-platform-server-nine.vercel.app'
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://task-management-platform-server-nine.vercel.app'
+    // baseURL: 'http://localhost:5000'
 })
 
 const useAxios = () => {
